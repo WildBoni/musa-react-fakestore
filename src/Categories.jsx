3191c9f33f6f066ebf5cf04fb34c1c843506f3c1
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 10:41:00 +0100

    categories component

---
+++ src/Categories.jsx
@@ -0,0 +1,10 @@
+export default function Categories() {
+  return (
+    <div>
+      <h3>Categories</h3>
+      <button>electronics</button>
+      <button>jewelery</button>
+      <button>men's clothing</button>
+    </div>
+  )
+}
