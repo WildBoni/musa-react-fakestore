bdb0b6363ee148390a6cef7d7d62ff92f7d32d31
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 13:36:03 +0100

    useEffect on category

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,7 +1,23 @@
-import categories from "./data/categories"
+import { useEffect, useState } from "react"
+// import categories from "./data/categories"
+// 'https://fakestoreapi.com/products/categories'
 
 export default function Categories({handleClick}) {
-  
+  let [categories, setCategories] = useState([]);
+
+  useEffect(
+    () => {
+      //funzione da eseguire
+      async function fetchCategories() {
+        let res = await fetch('https://fakestoreapi.com/products/categories');
+        let data = await res.json();
+        setCategories(data);
+      }
+      fetchCategories()
+    },
+    [] // array per chiamare useEffect
+  )
+
   let categoriesList = categories.map(category => 
     <button onClick={() => handleClick(category)} key={category}>
       {category}
