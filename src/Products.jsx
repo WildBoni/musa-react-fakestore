4b36a5a953e8279b821478a28b15b21db5d4c446
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 11:31:50 +0100

    loading products from array

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,10 +1,13 @@
+import products from './data/products';
 import Product from "./Product"
 
 export default function Products() {
+  let productList = products.map(product => <Product />)
+
   return (
     <div>
       <h2>MyProducts</h2>
-      <Product />
+      {productList}
     </div>
   )
 }
