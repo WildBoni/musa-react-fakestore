d52ddb61e3b682df07e2ba82622fa4e07193244c
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 14:15:56 +0100

    final example

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,12 +1,26 @@
-import products from './data/products';
+import { useEffect, useState } from 'react';
+// import products from './data/products';
 import Product from "./Product"
 
 export default function Products({category}) {
+  let [products, setProducts] = useState([]);
+
+  useEffect(
+    () => {
+      async function fetchProducts() {
+        let res = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+        let data = await res.json();
+        setProducts(data);
+      }
+      fetchProducts()
+    },
+    [category]
+  )
 
   let productList = products
-    .filter(
-      product => product.category === category
-    )
+    // .filter(
+    //   product => product.category === category
+    // )
     .map(
       product => <Product productDetails={product} key={product.id} />
     );
