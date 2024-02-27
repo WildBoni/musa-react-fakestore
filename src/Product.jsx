fd0a9a1c1c1c85dc27fa7918dd228828296920a5
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 12:15:47 +0100

    filtering products

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,7 +4,7 @@ export default function Product({productDetails}) {
     <article>
       <h3>{productDetails.title}</h3>
       <p>{productDetails.description}</p>
-      <img src={productDetails.image} alt="" />
+      <img src={productDetails.image} />
       <h3>{productDetails.price}</h3>
     </article>
   )
--- src/Products.jsx
+++ src/Products.jsx
@@ -2,9 +2,14 @@ import products from './data/products';
 import Product from "./Product"
 
 export default function Products() {
-  let productList = products.map(product => 
-    <Product productDetails={product} />
-  )
+
+  let productList = products
+    .filter(
+      product => product.category === 'electronics'
+    )
+    .map(
+      product => <Product productDetails={product} />
+    )
 
   return (
     <div>
