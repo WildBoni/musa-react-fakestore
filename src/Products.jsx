058ff90f02f100e946ef9eb1fa577a8ffa58db39
Davide Bonifacio <web@davidebonifacio.it>
2024-02-28 09:59:39 +0100

    Update Products.jsx

--- src/Products.jsx
+++ src/Products.jsx
@@ -4,26 +4,47 @@ import Product from "./Product"
 
 export default function Products({category}) {
   let [products, setProducts] = useState([]);
-
+  let [error, setError] = useState('');
+  let [isLoading, setIsLoading] = useState(false);
+  
   useEffect(
     () => {
       async function fetchProducts() {
-        let res = await fetch(`https://fakestoreapi.com/products/category/${category}`)
-        let data = await res.json();
-        setProducts(data);
+        setProducts('');
+        setIsLoading(true);
+        try {
+          let res = await fetch(`https://sdffakestoreapi.com/products/category/${category}`)
+          let data = await res.json();
+          setProducts(data);
+        } catch(error) {
+          setError('OPS, errore')
+        }
+        setIsLoading(false)
       }
       fetchProducts()
     },
     [category]
   )
 
-  let productList = products
+  let productList = <p>No products...</p>
+
+  if(isLoading) {
+    productList = <p>Loading...</p>
+  }
+
+  if(error) {
+    productList = <p>{error}</p>
+  }
+  
+  if(products.length > 0) {
+    productList = products
     // .filter(
     //   product => product.category === category
     // )
     .map(
       product => <Product productDetails={product} key={product.id} />
     );
+  }
 
   return (
     <div>
